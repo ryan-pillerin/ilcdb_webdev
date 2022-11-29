d8210055f297206878e1abee8ad64ab4e2f42914
Ryan Pillerin <ryan.david.pillerin@gmail.com>
2022-11-29 10:31:59 +0800

    NULL values

--- realtordb.sql
+++ realtordb.sql
@@ -11,3 +11,8 @@ CREATE TABLE `registration` (
 insert into registration(first_name, middle_name, last_name, username, password) values('John', 'John', 'Veltran', 'johnveltran', 'veltran');
 
 /* ALTER TABLE `registration` CHANGE `username` `username` VARCHAR(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL; */
+INSERT INTO `registration` (`ID`, `first_name`, `middle_name`, `last_name`, `address`, `username`, `password`, `last_updated`) VALUES (NULL, 'James', 'Yap', 'Uy', 'Davao City', 'jamesuy', 'james', current_timestamp());
+
+SELECT * FROM `registration` WHERE address IS NULL;
+SELECT * FROM `registration` WHERE address IS NOT NULL; 
+
